bf8e3fadf6c376d30e894eb11cfb4526b0787cf8
Micholn <mdiagbare@gmail.com>
2023-12-06 15:19:44 +0100

    reconfigured the next file

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ const { i18n } = require("./next-i18next.config");
 
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true"
-})
+    enabled: process.env.ANALYZE === "true",
+});
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
