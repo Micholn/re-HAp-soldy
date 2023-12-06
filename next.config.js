ab1c825ddf55b17f91f34e801539ca04a2224bd8
Micholn <mdiagbare@gmail.com>
2023-12-06 15:22:21 +0100

    reconfigured the next file

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({
     poweredByHeader: false,
     trailingSlash: true,
+    basePath: "",
 })
 
 // /** @type {import('next').NextConfig} */
