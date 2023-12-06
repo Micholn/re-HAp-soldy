06b7a470189014ed6b3aceec11ae4240ec3973ad
Micholn <mdiagbare@gmail.com>
2023-12-06 15:20:30 +0100

    reconfigured the next file

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
 
-module.exports 
+module.exports = withBundleAnalyzer({
+    
+})
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {}
