02132b84ab737690a48f5cb3573da895f6b0f369
Micholn <mdiagbare@gmail.com>
2023-12-06 15:24:54 +0100

    reconfigured the next file

--- next.config.js
+++ next.config.js
@@ -9,8 +9,12 @@ module.exports = withBundleAnalyzer({
     poweredByHeader: false,
     trailingSlash: true,
     basePath: "",
-    //
-    reactStrictMode: true
+    // The starter code load resources from `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    reactStrictMode: true,
+    i18n,
+    productionBrowserSourceMaps: true,
 })
 
 // /** @type {import('next').NextConfig} */
